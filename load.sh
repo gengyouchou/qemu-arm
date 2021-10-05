ab44af7ab0da6d4875410fadade86c96ec90e8c3
gengyouzhou <resisttempetation23@gmail.com>
2021-10-05 15:39:35 +0800

    second summit

--- load.sh
+++ load.sh
@@ -1,3 +1,26 @@
-sudo mount -o loop ./a9rootfs.ext4 tmpfs/  
-cp ./socket_arm/client tmpfs/ 
-sudo umount tmpfs 
+mkdir -p rootfs/dev
+mkdir -p rootfs/etc/init.d
+
+sudo mknod rootfs/dev/tty1 c 4 1
+sudo mknod rootfs/dev/tty2 c 4 2
+sudo mknod rootfs/dev/tty3 c 4 3
+sudo mknod rootfs/dev/tty4 c 4 4
+
+dd if=/dev/zero of=a9rootfs.ext4 bs=1M count=32
+
+mkfs.ext4 a9rootfs.ext4
+
+
+if [ -d "./tmpfs" ]; then
+    # 目錄 /path/to/dir 存在
+    echo "Directory ./tmpfs exists."
+else
+    # 目錄 /path/to/dir 不存在
+    sudo mkdir tmpfs
+fi
+
+
+sudo mount -t ext4 a9rootfs.ext4 tmpfs/ -o loop
+sudo cp -r rootfs/*  tmpfs/
+sudo umount tmpfs
+
