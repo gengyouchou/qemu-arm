a6a33f10b3f832919c3d1f7370cde20438dadc5b
gengyouchou <resisttempetation23@gmail.com>
2021-10-08 10:43:04 +0800

    add TFTP,NFS,u-image

--- qemu_start.sh
+++ qemu_start.sh
@@ -1,27 +1,24 @@
 # cat boot.sh
 #! /bin/sh
-VMN=1
+#VMN=1
 sudo qemu-system-arm \
 -M vexpress-a9 \
--smp sockets=1,cpus=4,cores=2 \
 -m 512M \
--kernel ./linux-5.10.70/object/arch/arm/boot/zImage \
--dtb ./linux-5.10.70/object/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
 -nographic \
--append "root=/dev/mmcblk0 rw console=ttyAMA0" \
--sd a9rootfs.ext4 \
-<<<<<<< Updated upstream
--netdev user,id=mynet0,hostfwd=tcp::${VMN}0022-:22,hostfwd=tcp::${VMN}2375-:2375 \
--device virtio-net-device,netdev=mynet0 \
--netdev socket,id=vlan,mcast=230.0.0.1:1234 \
--device virtio-net-device,netdev=vlan
-#-netdev tap,id=mynet0
+-net nic -net tap,ifname=tap0,script=no \
+-kernel /home/dptemp/arm/u-boot-2018.11/u-boot \
+#-dtb ./linux-5.10.70/object/arch/arm/boot/dts/vexpress-v2p-ca9.dtb
+#-sd a9rootfs.ext4
+#-drive if=pflash,format=raw,index=1,file=/home/dptemp/TFTP_HOME/flash.img
+#-smp sockets=1,cpus=4,cores=2 \
 
-=======
--net nic -net tap,ifname=tap0,script=no
+#-dtb ./linux-5.10.70/object/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
+
+#-append "root=/dev/mmcblk0 rw console=ttyAMA0" \
+#-sd a9rootfs.ext4 \
+
 #-netdev user,id=mynet0,hostfwd=tcp::${VMN}0023-:23,hostfwd=tcp::${VMN}2376-:2376 \
 #-device virtio-net-device,netdev=mynet0 \
 #-netdev socket,id=vlan,mcast=230.0.0.1:1234 \
 #-device virtio-net-device,netdev=vlan
 #-netdev tap,id=mynet0
->>>>>>> Stashed changes
--- rootfs/socket_arm/add_net_bridge.sh
+++ rootfs/socket_arm/add_net_bridge.sh
@@ -8,4 +8,13 @@ sudo tunctl -t tap0 -u dpnums
 sudo ifconfig tap0 10.8.8.4 netmask 255.255.255.0 promisc up
 sudo brctl addif br0 tap0
 
-#setenv ipaddr 10.8.8.100; setenv netmask 255.255.255.0; setenv serverip 10.8.8.3;
+setenv ipaddr 10.8.8.100; setenv netmask 255.255.255.0; setenv serverip 10.8.8.3;
+setenv bootargs 'root=/dev/nfs rw nfsroot=10.8.8.3:/home/dptemp/NFS_HOME init=/linuxrc ip=10.8.8.100 console=ttyAMA0';
+saveenv;
+
+ping 10.8.8.3;
+
+tftp 0x60003000 uImage;
+tftp 0x62003000 vexpress-v2p-ca9.dtb;
+
+bootm 0x60003000 - 0x62003000;
